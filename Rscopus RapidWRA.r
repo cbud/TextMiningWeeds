ef719cdb8d68f2121659a3fa45ad58cf23b10eb8
Ben Wynne-Jones <Ben.Wynne-Jones@agresearch.co.nz>
2022-07-19 09:16:35 +1200

    scopus import and tidy data

--- Rscopus RapidWRA.r
+++ Rscopus RapidWRA.r
@@ -1,46 +1,100 @@
-#rscopus API key 	d2dcc4a928a10f0d9184f6ced6473c3c 
+#rscopus API key 	d2dcc4a928a10f0d9184f6ced6473c3c
 
 #https://aurelien-goutsmedt.com/post/extracting-biblio-data-1/
 #
 library(rscopus)
 library(bibliometrix)
+library(tidyverse)
 
-set_api_key("d2dcc4a928a10f0d9184f6ced6473c3c" )
-#res = author_df(last_name = "Buddenhagen", first_name = "C", verbose = TRUE, general = FALSE)
+# Import and reformat Scopus data -----------------------------------------
 
-# Species_full<-"Arundo donax"
-# 
-# weed_query <- rscopus::scopus_search("TITLE-ABS-KEY(weed OR invasiv*) AND TITLE-ABS-KEY(\"Arundo donax\") OR TITLE-ABS-KEY(\"A donax\")", 
-#                                      view = "COMPLETE")
-# 
+set_api_key("d2dcc4a928a10f0d9184f6ced6473c3c")
+                 
+#create list of weeds to search
+weed_list <- c("Lolium perenne", "Arundo donax")
+
+#create lists of abbreviated names
+weed_list_abr <-  paste0(substr(weed_list, 1, 1), ". ",sub("^\\S+\\s+", '', weed_list))
+weed_list_abr2 <-  paste0(substr(weed_list, 1, 1), " ",sub("^\\S+\\s+", '', weed_list))
+
+#makes the string for Scopus query
+QueryList <-
+  paste0("TITLE-ABS-KEY(weed* OR invasi*) AND TITLE-ABS-KEY({", weed_list, "} OR {", weed_list_abr, "} OR {", weed_list_abr2, "})")
+
+#queries one of the species in list
+# weed_query <- rscopus::scopus_search(QueryList[2], view = "COMPLETE")
 # weed_data_raw <- gen_entries_to_df(weed_query$entries)
 # weed_papers <- weed_data_raw$df
-# weed_affiliations <- weed_data_raw$affiliation 
+# weed_affiliations <- weed_data_raw$affiliation
+# weed_authors <- weed_data_raw$author
+
+#queries each species in list and adds to data table
+for (i in 1:length(weed_list)) {
+  weed_query <-
+    rscopus::scopus_search(QueryList[i], view = "COMPLETE")
+  weed_data_raw <- gen_entries_to_df(weed_query$entries)
+  species_papers<-weed_data_raw$df
+  species_papers$weed_searched<- weed_list[i]
+  if (exists("weed_papers") == F) {
+   weed_papers <- species_papers
+  }
+  else{
+   weed_papers <- bind_rows(weed_papers, species_papers)
+  }
+}
+
+#remove special characters from column names
+names(weed_papers) <- names(weed_papers) %>% 
+  {gsub("prism:", "", .)}%>%
+  {gsub("dc:", "", .)}%>%
+  {gsub("-", "_", .)}%>%
+  {gsub("@", "_", .)}%>%
+  {gsub("[.$]", "", .)}
+
+#removes duplicates based on weeds searched + title
+weed_papers<-weed_papers %>%
+  distinct(paste(tolower(title)," ",weed_searched), .keep_all=TRUE) %>% 
+  select(-"paste(tolower(title), \" \", weed_searched)")
+ 
+#adds lowercase title, abstract, and keywords to the "combined" column                                  
+weed_papers$combined <- paste(tolower(weed_papers$title), tolower(weed_papers$description), tolower(weed_papers$authkeywords))
+
+#creates "keep" column
+weed_papers<- weed_papers %>% mutate(keep=TRUE)
+
+
+#counts papers included before cleaning
+before_cleaning<- weed_papers %>% 
+  group_by(weed_searched) %>% 
+  summarise(n()) 
+
+
+# Clean data -----------------------------------------
+
+#cleaning goes here (by setting keep=FALSE for bad papers)
+
+
+
+
+
+# Output -----------------------------------------
+
+
+after_cleaning<- weed_papers %>% 
+  filter(keep==TRUE) %>%
+  group_by(weed_searched) %>% 
+  summarise(n())
+
+before_cleaning
+after_cleaning
+
+#res = author_df(last_name = "Buddenhagen", first_name = "C", verbose = TRUE, general = FALSE)
+
+# weed_data_raw <- gen_entries_to_df(weed_query$entries)
+# weed_papers <- weed_data_raw$df
+# weed_affiliations <- weed_data_raw$affiliation
 # weed_authors <- weed_data_raw$author
 
 # https://johnmuschelli.com/rscopus/articles/multi_author.html
 
 #maybe adapt this code to query the species one by one?
-
-
-
-weed_list<-c("A donax", "A riparia", "Avena fatua")
-
-#Makes a list of queries for each species.
-
-QueryList <- paste0("TITLE-ABS-KEY(weed* OR invasi*) AND TITLE-ABS-KEY(", 
-                weed_list, 
-                ")")
-QueryList
-
-#queries one of the species in list
-
-weed_query <- rscopus::scopus_search(QueryList[2], 
-                                     view = "COMPLETE")
-
-weed_data_raw2 <- gen_entries_to_df(weed_query$entries)
-weed_papers2 <- weed_data_raw2$df
-weed_affiliations2 <- weed_data_raw2$affiliation 
-weed_authors2 <- weed_data_raw2$author
-
-
