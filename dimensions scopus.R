8bed25999da2f2f61cc4a1ea354738a8469a197e
Ben Wynne-Jones <Ben.Wynne-Jones@agresearch.co.nz>
2022-08-18 10:38:32 +1200

    added dimensions_scopus

--- dimensions scopus.R
+++ dimensions scopus.R
@@ -5,6 +5,8 @@ library(tictoc)
 library(bib2df)
 library(rscopus)
 library(bibliometrix)
+library(readxl)
+
 
 #starts timer
 tic()
@@ -18,21 +20,28 @@ hdr<-inst_token_header("272404b5b445f8a89b33cda259416973" )
 
 
 
+weed_list <- read_excel("Small Data Files/gbif_all_synonyms_with_occurrence_counts_282_weeds_20220311.xlsx")
+weed_list <- weed_list$`Row Labels`
+
 #create list of weeds to search
-weed_list <- c("Cirsium vulgare", "Cuscuta campestris")
+#weed_list <- c("Cirsium vulgare", "Cuscuta campestris")
+
 
 #create lists of abbreviated names
 weed_list_abr <-  paste0(substr(weed_list, 1, 1), ". ",sub("^\\S+\\s+", '', weed_list))
 weed_list_abr2 <-  paste0(substr(weed_list, 1, 1), " ",sub("^\\S+\\s+", '', weed_list))
 
 #creates the query string for dimensions
-dimensions_query <- paste0("search publications in full_data  for \"(\\\"weed\\\"OR\\\"invasi*\\\"OR\\\"introduced species\\\"OR\\\"invasive species\\\"OR\\\"invasive organisms\\\"OR\\\"alien invasive species\\\"OR\\\"invasive alien species\\\"OR\\\"weed control\\\")AND(\\\"",weed_list,"\\\"OR\\\"",weed_list_abr,"\\\"OR\\\"",weed_list_abr2,"\\\")\" where year in [ 1900 : 2022 ] and type in [ \"article\" ] return publications[type + basics + extras + authors + concepts + abstract]")
+
+dimensions_query <- paste0("search publications in title_abstract_only for \"(\\\"weed\\\"OR\\\"invasi*\\\"OR\\\"introduced species\\\"OR\\\"invasive species\\\"OR\\\"invasive organisms\\\"OR\\\"alien invasive species\\\"OR\\\"invasive alien species\\\"OR\\\"weed control\\\")AND(\\\"",weed_list,"\\\"OR\\\"",weed_list_abr,"\\\"OR\\\"",weed_list_abr2,"\\\")\" where year in [ 1900 : 2022 ] and type in [ \"article\" ] return publications[type + basics + extras + authors + concepts + abstract]")
 
 #queries each species in list and adds to data table
 for (i in 1:length(weed_list)) {
-  query_result <- dsApiRequest(token = token, query = dimensions_query[i], step = 200, limit = 10000)
+  query_result <- dsApiRequest(token = token, query = dimensions_query[i], step = 200, limit = 50000)
   ds_temp <- dsApi2df(query_result)
   ds_temp$weed_searched<- weed_list[i]
+  paste0("Searched species ", i, " of ", length(weed_list), " (", weed_list[i], ").\n")
+  
   if (i == 1) {
     #  if (exists("weed_papers") == F) {
     dimensions_raw <- ds_temp
@@ -149,12 +158,15 @@ remove_words <- function(input) {
 papers<-keep_words(list("weed"))
 
 #runs function to exclude papers with these words anywhere
-papers<-remove_words(list("insect pest", "breeding","pests", "wildlife","medicin","medical", "cancer",  "carnivore", "domesticated", "folk", "herbal", "essential oils"))
+
+papers<-remove_words(list("cover crop performance", "insect pest", "pests","breeding","pests","medicin","medical", "cancer",  "carnivore", "domesticated", "folk", "herbal", "essential oils", "cm soil", "soil organic matter"))
+
 
 # Section 3 Output -----------------------------------------
 
 #creates summary of kept papers by counting them, grouped by status and reason
-kept_summary<- papers %>% 
+
+kept_summary <- papers %>% 
   group_by(status, reason) %>% 
   summarise(n()) 
 
@@ -163,7 +175,10 @@ kept_summary<- papers %>%
 names(kept_summary) <- c("kept papers", "reason", "count")
 
 #workaround that removes quotation marks that appeared in the reason column
-kept_summary<-kept_summary %>% mutate(reason=noquote(reason))
+
+kept_summary <- kept_summary %>% mutate(reason = noquote(reason))
+
+
 
 #creates summary of removed papers by counting them, grouped by status and reason
 removed_summary <- removed_papers %>% 
@@ -175,7 +190,7 @@ names(removed_summary) <- c("removed papers", "reason", "count")
 
 removed_summary
 
-count(papers, weed_searched)
+count_weeds<- count(papers, weed_searched)
 
 #displays run time
 toc()
